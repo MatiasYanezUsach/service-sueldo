a7c1d97c80be59e34190dca26413c3ea20479a60
Matias Yanez <matias.yanez@usach.cl>
2022-11-05 15:02:08 -0300

    inicializacion del microservicio suelfo

---
+++ src/main/java/proyecto/mingeso/microservicesueldo/MicroserviceSueldoApplication.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microservicesueldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSueldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSueldoApplication.class, args);
+	}
+
+}
