a2e3a17343e707dfc0db7aad88d1d9f396bb8686
Matias Yanez <matias.yanez@usach.cl>
2022-11-20 20:02:49 -0300

    crossOrigin

--- src/main/java/proyecto/mingeso/microservicesueldo/controllers/SueldoController.java
+++ src/main/java/proyecto/mingeso/microservicesueldo/controllers/SueldoController.java
@@ -2,6 +2,7 @@ package proyecto.mingeso.microservicesueldo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/sueldo")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SueldoController {
     @Autowired
     SueldoService sueldoService;
